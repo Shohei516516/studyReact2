68229282bafd933bfc613878daa527e7b4f56786
Shohei <126993200+Shohei516516@users.noreply.github.com>
2023-06-09 14:29:21 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
+import { ChildArea } from "./childArea";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const onClickCountUp = () => setCount(count + 1);
+  const onClickSetText = () => {};
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input />
+      <br />
+      <br />
+      <button>表示</button>
+      <ChildArea />
     </div>
   );
 }
